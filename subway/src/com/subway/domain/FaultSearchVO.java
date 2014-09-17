827357f3c82bdd9fedc32de815e649b5b645f769
liyanfeng <2573746941@qq.com>
2014-09-17 12:54:55 +0800

    add new files

---
+++ subway/src/com/subway/domain/FaultSearchVO.java
@@ -0,0 +1,115 @@
+package com.subway.domain;
+
+import java.util.Date;
+
+public class FaultSearchVO {
+	private String grade;
+	private String code;
+	private String status;
+	private String major;
+	private String userowner;
+	private String device;
+	private String place;
+	
+	private String finder;
+	private String accepter;
+	
+	private String isConfirm;
+	private String cause;
+	
+	private String timestart;
+	private String timeend;
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getUserowner() {
+		return userowner;
+	}
+	public void setUserowner(String userowner) {
+		this.userowner = userowner;
+	}
+	public String getDevice() {
+		return device;
+	}
+	public void setDevice(String device) {
+		this.device = device;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getFinder() {
+		return finder;
+	}
+	public void setFinder(String finder) {
+		this.finder = finder;
+	}
+	public String getAccepter() {
+		return accepter;
+	}
+	public void setAccepter(String accepter) {
+		this.accepter = accepter;
+	}
+	public String getIsConfirm() {
+		return isConfirm;
+	}
+	public void setIsConfirm(String isConfirm) {
+		this.isConfirm = isConfirm;
+	}
+	public String getCause() {
+		return cause;
+	}
+	public void setCause(String cause) {
+		this.cause = cause;
+	}
+	public String getTimestart() {
+		return timestart;
+	}
+	public void setTimestart(String timestart) {
+		this.timestart = timestart;
+	}
+	public String getTimeend() {
+		return timeend;
+	}
+	public void setTimeend(String timeend) {
+		this.timeend = timeend;
+	}
+	
+	
+	
+		
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+}
